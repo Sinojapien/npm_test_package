8181876284f24c876b71473363ede2d0c7e5d135
Sinojapien <1155126460@link.cuhk.edu.hk>
2022-06-27 16:16:19 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import BigButton from './components/BigButton';
 
-class ImageGallery extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div>
-        <h1>Bye!</h1>
-        <div><BigButton/></div>
-        <div><BigButton/></div>
-        <div><BigButton/></div>
-      </div>
-    );
-  }
+// class ImageGallery extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>Bye!</h1>
+//         <div><BigButton/></div>
+//         <div><BigButton/></div>
+//         <div><BigButton/></div>
+//       </div>
+//     );
+//   }
+// };
+
+const ImageGallery = () =>{
+  return (
+    <div>
+      <h1>Goof!</h1>
+      <div><BigButton/></div>
+      <div><BigButton/></div>
+      <div><BigButton/></div>
+    </div>
+  )
 };
 
 export default ImageGallery;
