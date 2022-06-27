562825159d3ed5431333839ebc4784fb372949fe
Sinojapien <1155126460@link.cuhk.edu.hk>
2022-06-27 17:10:24 +0800

    init

--- src/index.js
+++ src/index.js
@@ -2,29 +2,12 @@ import React from 'react';
 import { useEffect } from "react";
 import BigButton from './components/BigButton';
 
-// class ImageGallery extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
-//   render() {
-//     return (
-//       <div>
-//         <h1>Bye!</h1>
-//         <div><BigButton/></div>
-//         <div><BigButton/></div>
-//         <div><BigButton/></div>
-//       </div>
-//     );
-//   }
-// };
-
 const ImageGallery = () =>{
 
   useEffect(() => {console.log(123)}, []);
 
   return (
-    <div>
-      <h1>Goof!</h1>
+    <div class="test-component">
       <div><BigButton/></div>
       <div><BigButton/></div>
       <div><BigButton/></div>
