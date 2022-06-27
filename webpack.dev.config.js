2ed3cf9f99c13351914e9d6a08b39018e6bd2a37
Sinojapien <1155126460@link.cuhk.edu.hk>
2022-06-27 15:51:13 +0800

    init

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -61,4 +61,19 @@ module.exports = {
     }),
     new MiniCssExtractPlugin(),
   ],
+  // externals: {
+  //   // Don't bundle react or react-dom
+  //   react: {
+  //     commonjs: 'react',
+  //     commonjs2: 'react',
+  //     amd: 'react',
+  //     root: 'React',
+  //   },
+  //   'react-dom': {
+  //     commonjs: 'react-dom',
+  //     commonjs2: 'react-dom',
+  //     amd: 'react-dom',
+  //     root: 'ReactDOM',
+  //   },
+  // },
 };
