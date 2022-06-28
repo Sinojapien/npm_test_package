9cb1fd0c54a2547bdea45fa38c98ea2e82f70ab1
Sinojapien <1155126460@link.cuhk.edu.hk>
2022-06-28 12:41:22 +0800

    init

--- src/index.js
+++ src/index.js
@@ -2,10 +2,29 @@ import React from 'react';
 import { useEffect } from "react";
 import BigButton from './components/BigButton';
 
-const ImageGallery = () =>{
+// Cannot export function directly (empty index.bundle.js file)
+// class TestComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
 
+//   render() {
+//     // useEffect(() => {console.log(123)}, []);
+
+//     return (
+//       <div>
+//         <h1>Bye!</h1>
+//         <div><BigButton/></div>
+//         <div><BigButton/></div>
+//         <div><BigButton/></div>
+//       </div>
+//     );
+//   }
+// };
+
+// https://stackoverflow.com/questions/45817227/how-to-export-function-with-webpack
+const TestComponent = () =>{
   useEffect(() => {console.log(123)}, []);
-
   return (
     <div class="test-component">
       <div><BigButton/></div>
@@ -15,5 +34,5 @@ const ImageGallery = () =>{
   )
 };
 
-export default ImageGallery;
+export default TestComponent;
 
